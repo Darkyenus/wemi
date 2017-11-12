6f5e712a3364997d627d48e1f7268e39e0a9e48c
Darkyenus <darkyenus@gmail.com>
2017-11-12 14:07:10 +0100

    Use unified settings for run-configuration and before-run-configuration
    Took 25 minutes

--- ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/execution/WemiBeforeRunTaskProvider.kt
+++ ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/execution/WemiBeforeRunTaskProvider.kt
@@ -1,19 +1,26 @@
 package com.darkyen.wemi.intellij.execution
 
 import com.darkyen.wemi.intellij.WemiProjectSystemId
+import com.darkyen.wemi.intellij.execution.configurationEditor.WemiTaskSettingsControl
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemBeforeRunTask
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemBeforeRunTaskProvider
+import com.intellij.openapi.externalSystem.util.ExternalSystemBundle
+import com.intellij.openapi.externalSystem.util.PaintAwarePanel
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.Key
 import icons.WemiIcons
+import java.awt.GridBagLayout
+import javax.swing.JComponent
 
 /**
  * Provides ability to run Wemi task before any run configuration.
  *
- * The task currently looks like the Wemi Run Configuration. (TODO: Is this intentional? What is it caused by?)
+ * The task uses the same settings controller as the Wemi Run Configuration.
  */
-class WemiBeforeRunTaskProvider(project: Project) : ExternalSystemBeforeRunTaskProvider(WemiProjectSystemId, project, ID) {
+class WemiBeforeRunTaskProvider(private val project: Project) : ExternalSystemBeforeRunTaskProvider(WemiProjectSystemId, project, ID) {
 
     /** Icon used in the "Before launch" window in "Run/Debug Configurations" */
     override fun getIcon() = WemiIcons.WEMI
@@ -26,7 +33,50 @@ class WemiBeforeRunTaskProvider(project: Project) : ExternalSystemBeforeRunTaskP
      */
     override fun createTask(configuration: RunConfiguration) = ExternalSystemBeforeRunTask(ID, WemiProjectSystemId)
 
+    override fun configureTask(runConfiguration: RunConfiguration, task: ExternalSystemBeforeRunTask): Boolean {
+        val dialog = WemiEditTaskDialog(project, task.taskExecutionSettings)
+        return dialog.showAndGet()
+    }
+
     companion object {
         val ID = Key.create<ExternalSystemBeforeRunTask>("Wemi.BeforeRunTask")
+    }
+
+    private class WemiEditTaskDialog(project: Project,
+                                     private val myTaskExecutionSettings: ExternalSystemTaskExecutionSettings) : DialogWrapper(project, true) {
+        private val myControl: WemiTaskSettingsControl
+        private var contentPane: JComponent? = null
+
+        init {
+
+            title = ExternalSystemBundle.message("tasks.select.task.title", WemiProjectSystemId.readableName)
+            myControl = WemiTaskSettingsControl(project)
+            myControl.setOriginalSettings(myTaskExecutionSettings)
+            isModal = true
+            init()
+        }
+
+        override fun createCenterPanel(): JComponent? {
+            if (contentPane == null) {
+                contentPane = PaintAwarePanel(GridBagLayout())
+                myControl.fillUi((contentPane as PaintAwarePanel?)!!, 0)
+                myControl.reset()
+            }
+            return contentPane
+        }
+
+        override fun getPreferredFocusedComponent(): JComponent? {
+            return null
+        }
+
+        override fun dispose() {
+            super.dispose()
+            myControl.disposeUIResources()
+        }
+
+        override fun doOKAction() {
+            myControl.apply(myTaskExecutionSettings)
+            super.doOKAction()
+        }
     }
 }
--- ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/execution/configurationEditor/WemiTaskSettingsControl.java
+++ ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/execution/configurationEditor/WemiTaskSettingsControl.java
@@ -13,19 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.darkyen.wemi.intellij.execution.configurationEditor;
 
 import com.darkyen.wemi.intellij.WemiKt;
+import com.darkyen.wemi.intellij.manager.WemiManager;
 import com.intellij.execution.configuration.EnvironmentVariablesComponent;
 import com.intellij.execution.configuration.EnvironmentVariablesData;
-import com.intellij.openapi.externalSystem.ExternalSystemManager;
-import com.intellij.openapi.externalSystem.ExternalSystemUiAware;
 import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings;
 import com.intellij.openapi.externalSystem.service.execution.TaskCompletionProvider;
 import com.intellij.openapi.externalSystem.service.ui.ExternalProjectPathField;
 import com.intellij.openapi.externalSystem.util.*;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
-import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.fileTypes.PlainTextFileType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
@@ -43,106 +42,106 @@ import java.awt.*;
 
 import static com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.normalizePath;
 
-/**
- * TODO Use also in ExternalSystemEditTaskDialog
- */
+/** Controls editing of standard ExternalSystemTaskExecutionSettings but in a way that makes sense for Wemi.
+ *
+ * Modifications:
+ * - Arguments are not used */
 public class WemiTaskSettingsControl implements ExternalSystemSettingsControl<ExternalSystemTaskExecutionSettings> {
 
-  @NotNull private final Project myProject;
+	@NotNull private final Project myProject;
 
-  @SuppressWarnings("FieldCanBeLocal") // Used via reflection at showUi() and disposeResources()
-  private JBLabel myProjectPathLabel;
-  private ExternalProjectPathField myProjectPathField;
-  @SuppressWarnings("FieldCanBeLocal") // Used via reflection at showUi() and disposeResources()
-  private JBLabel myTasksLabel;
-  private EditorTextField myTasksTextField;
-  @SuppressWarnings("FieldCanBeLocal") // Used via reflection at showUi() and disposeResources()
-  private JBLabel myVmOptionsLabel;
-  private RawCommandLineEditor myVmOptionsEditor;
-  private EnvironmentVariablesComponent myEnvVariablesComponent;
+	@SuppressWarnings("FieldCanBeLocal")// Used via reflection at showUi() and disposeResources()
+	private JBLabel myProjectPathLabel;
+	private ExternalProjectPathField myProjectPathField;
+	@SuppressWarnings("FieldCanBeLocal")// Used via reflection at showUi() and disposeResources()
+	private JBLabel myTasksLabel;
+	private EditorTextField myTasksTextField;
+	@SuppressWarnings("FieldCanBeLocal")// Used via reflection at showUi() and disposeResources()
+	private JBLabel myVmOptionsLabel;
+	private RawCommandLineEditor myVmOptionsEditor;
+	private EnvironmentVariablesComponent myEnvVariablesComponent;
 
-  @Nullable private ExternalSystemTaskExecutionSettings myOriginalSettings;
+	@Nullable private ExternalSystemTaskExecutionSettings myOriginalSettings;
 
-  public WemiTaskSettingsControl(@NotNull Project project) {
-    myProject = project;
-  }
+	public WemiTaskSettingsControl (@NotNull Project project) {
+		myProject = project;
+	}
 
-  public void setOriginalSettings(@Nullable ExternalSystemTaskExecutionSettings originalSettings) {
-    myOriginalSettings = originalSettings;
-  }
+	public void setOriginalSettings (@Nullable ExternalSystemTaskExecutionSettings originalSettings) {
+		myOriginalSettings = originalSettings;
+	}
 
-  @Override
-  public void fillUi(@NotNull final PaintAwarePanel canvas, int indentLevel) {
-    myProjectPathLabel = new JBLabel(ExternalSystemBundle.message(
-      "run.configuration.settings.label.project", WemiKt.getWemiProjectSystemId().getReadableName()
-    ));
-    ExternalSystemManager<?, ?, ?, ?, ?> manager = ExternalSystemApiUtil.getManager(WemiKt.getWemiProjectSystemId());
-    FileChooserDescriptor projectPathChooserDescriptor = null;
-    if (manager instanceof ExternalSystemUiAware) {
-      projectPathChooserDescriptor = ((ExternalSystemUiAware)manager).getExternalProjectConfigDescriptor();
-    }
-    if (projectPathChooserDescriptor == null) {
-      projectPathChooserDescriptor = FileChooserDescriptorFactory.createSingleLocalFileDescriptor();
-    }
-    String title = ExternalSystemBundle.message("settings.label.select.project", WemiKt.getWemiProjectSystemId().getReadableName());
-    myProjectPathField = new ExternalProjectPathField(myProject, WemiKt.getWemiProjectSystemId(), projectPathChooserDescriptor, title) {
-      @Override
-      public Dimension getPreferredSize() {
-        return myVmOptionsEditor == null ? super.getPreferredSize() : myVmOptionsEditor.getTextField().getPreferredSize();
-      }
-    };
-    canvas.add(myProjectPathLabel, ExternalSystemUiUtil.getLabelConstraints(0));
-    canvas.add(myProjectPathField, ExternalSystemUiUtil.getFillLineConstraints(0));
+	@Override
+	public void fillUi (@NotNull final PaintAwarePanel canvas, int indentLevel) {
+		myProjectPathLabel = new JBLabel(ExternalSystemBundle.message("run.configuration.settings.label.project", WemiKt
+			.getWemiProjectSystemId().getReadableName()));
+		final WemiManager manager = (WemiManager)ExternalSystemApiUtil.getManager(WemiKt.getWemiProjectSystemId());
+		assert manager != null;
+		final FileChooserDescriptor projectPathChooserDescriptor = manager.getExternalProjectConfigDescriptor();
+		assert projectPathChooserDescriptor != null;
 
-    myTasksLabel = new JBLabel(ExternalSystemBundle.message("run.configuration.settings.label.tasks"));
-    myTasksTextField = new EditorTextField("", myProject, PlainTextFileType.INSTANCE);
-    canvas.add(myTasksLabel, ExternalSystemUiUtil.getLabelConstraints(0));
-    GridBag c = ExternalSystemUiUtil.getFillLineConstraints(0);
-    c.insets.right = myProjectPathField.getButton().getPreferredSize().width + 8 /* street magic, sorry */;
-    canvas.add(myTasksTextField, c);
+		String title = ExternalSystemBundle.message("settings.label.select.project", WemiKt.getWemiProjectSystemId()
+			.getReadableName());
+		myProjectPathField = new ExternalProjectPathField(myProject, WemiKt.getWemiProjectSystemId(), projectPathChooserDescriptor,
+			title) {
+			@Override
+			public Dimension getPreferredSize () {
+				return myVmOptionsEditor == null ? super.getPreferredSize() : myVmOptionsEditor.getTextField().getPreferredSize();
+			}
+		};
+		canvas.add(myProjectPathLabel, ExternalSystemUiUtil.getLabelConstraints(0));
+		canvas.add(myProjectPathField, ExternalSystemUiUtil.getFillLineConstraints(0));
 
-    new TaskCompletionProvider(myProject, WemiKt.getWemiProjectSystemId(), myProjectPathField).apply(myTasksTextField);
+		myTasksLabel = new JBLabel(ExternalSystemBundle.message("run.configuration.settings.label.tasks"));
+		myTasksTextField = new EditorTextField("", myProject, PlainTextFileType.INSTANCE);
+		canvas.add(myTasksLabel, ExternalSystemUiUtil.getLabelConstraints(0));
+		GridBag c = ExternalSystemUiUtil.getFillLineConstraints(0);
+		c.insets.right = myProjectPathField.getButton().getPreferredSize().width + 8 /* street magic, sorry */;
+		canvas.add(myTasksTextField, c);
 
-    myVmOptionsLabel = new JBLabel(ExternalSystemBundle.message("run.configuration.settings.label.vmoptions"));
-    myVmOptionsEditor = new RawCommandLineEditor();
-    myVmOptionsEditor.setDialogCaption(ExternalSystemBundle.message("run.configuration.settings.label.vmoptions"));
-    canvas.add(myVmOptionsLabel, ExternalSystemUiUtil.getLabelConstraints(0));
-    canvas.add(myVmOptionsEditor, ExternalSystemUiUtil.getFillLineConstraints(0));
-    myEnvVariablesComponent = new EnvironmentVariablesComponent();
-    JBLabel myEnvVariablesComponentLabel = myEnvVariablesComponent.getLabel();
-    myEnvVariablesComponentLabel.remove(myEnvVariablesComponentLabel);
-    canvas.add(myEnvVariablesComponentLabel, ExternalSystemUiUtil.getLabelConstraints(0));
-    canvas.add(myEnvVariablesComponent, ExternalSystemUiUtil.getFillLineConstraints(0));
-  }
+		new TaskCompletionProvider(myProject, WemiKt.getWemiProjectSystemId(), myProjectPathField).apply(myTasksTextField);
 
-  @Override
-  public void reset() {
-    myProjectPathField.setText("");
-    myTasksTextField.setText("");
-    myVmOptionsEditor.setText("");
-    myEnvVariablesComponent.setEnvData(EnvironmentVariablesData.DEFAULT);
-    showUi(true);
+		myVmOptionsLabel = new JBLabel(ExternalSystemBundle.message("run.configuration.settings.label.vmoptions"));
+		myVmOptionsEditor = new RawCommandLineEditor();
+		myVmOptionsEditor.setDialogCaption(ExternalSystemBundle.message("run.configuration.settings.label.vmoptions"));
+		canvas.add(myVmOptionsLabel, ExternalSystemUiUtil.getLabelConstraints(0));
+		canvas.add(myVmOptionsEditor, ExternalSystemUiUtil.getFillLineConstraints(0));
+		myEnvVariablesComponent = new EnvironmentVariablesComponent();
+		JBLabel myEnvVariablesComponentLabel = myEnvVariablesComponent.getLabel();
+		myEnvVariablesComponentLabel.remove(myEnvVariablesComponentLabel);
+		canvas.add(myEnvVariablesComponentLabel, ExternalSystemUiUtil.getLabelConstraints(0));
+		canvas.add(myEnvVariablesComponent, ExternalSystemUiUtil.getFillLineConstraints(0));
+	}
 
-    if (myOriginalSettings == null) {
-      return;
-    }
+	@Override
+	public void reset () {
+		myProjectPathField.setText("");
+		myTasksTextField.setText("");
+		myVmOptionsEditor.setText("");
+		myEnvVariablesComponent.setEnvData(EnvironmentVariablesData.DEFAULT);
+		showUi(true);
 
-    String path = myOriginalSettings.getExternalProjectPath();
-    if (StringUtil.isEmpty(path)) {
-      path = "";
-    }
-    myProjectPathField.setText(path);
-    myTasksTextField.setText(StringUtil.join(myOriginalSettings.getTaskNames(), " "));
-    myVmOptionsEditor.setText(myOriginalSettings.getVmOptions());
-    myEnvVariablesComponent.setEnvData(
-      EnvironmentVariablesData.create(myOriginalSettings.getEnv(), myOriginalSettings.isPassParentEnvs()));
-  }
+		if (myOriginalSettings == null) {
+			return;
+		}
 
-  @Override
-  public boolean isModified() {
-    if (myOriginalSettings == null) {
-      return false;
-    }
+		String path = myOriginalSettings.getExternalProjectPath();
+		if (StringUtil.isEmpty(path)) {
+			path = "";
+		}
+		myProjectPathField.setText(path);
+		myTasksTextField.setText(StringUtil.join(myOriginalSettings.getTaskNames(), " "));
+		myVmOptionsEditor.setText(myOriginalSettings.getVmOptions());
+		myEnvVariablesComponent.setEnvData(EnvironmentVariablesData.create(myOriginalSettings.getEnv(),
+			myOriginalSettings.isPassParentEnvs()));
+	}
+
+	@Override
+	public boolean isModified () {
+		// noinspection SimplifiableIfStatement
+		if (myOriginalSettings == null) {
+			return false;
+		}
 
 		return !Comparing.equal(normalizePath(myProjectPathField.getText()),
 			normalizePath(myOriginalSettings.getExternalProjectPath()))
@@ -152,38 +151,38 @@ public class WemiTaskSettingsControl implements ExternalSystemSettingsControl<Ex
 			|| myEnvVariablesComponent.isPassParentEnvs() != myOriginalSettings.isPassParentEnvs()
 			|| !myEnvVariablesComponent.getEnvs().equals(myOriginalSettings.getEnv());
 
-  }
+	}
 
-  @Override
-  public void apply(@NotNull ExternalSystemTaskExecutionSettings settings) {
-    String projectPath = myProjectPathField.getText();
-    settings.setExternalProjectPath(projectPath);
-    settings.setTaskNames(StringUtil.split(myTasksTextField.getText(), " "));
-    settings.setVmOptions(myVmOptionsEditor.getText());
-    settings.setPassParentEnvs(myEnvVariablesComponent.isPassParentEnvs());
-    settings.setEnv(ContainerUtil.newHashMap(myEnvVariablesComponent.getEnvs()));
-  }
+	@Override
+	public void apply (@NotNull ExternalSystemTaskExecutionSettings settings) {
+		String projectPath = myProjectPathField.getText();
+		settings.setExternalProjectPath(projectPath);
+		settings.setTaskNames(StringUtil.split(myTasksTextField.getText(), " "));
+		settings.setVmOptions(myVmOptionsEditor.getText());
+		settings.setPassParentEnvs(myEnvVariablesComponent.isPassParentEnvs());
+		settings.setEnv(ContainerUtil.newHashMap(myEnvVariablesComponent.getEnvs()));
+	}
 
-  @Override
-  public boolean validate(@NotNull ExternalSystemTaskExecutionSettings settings) throws ConfigurationException {
-    String projectPath = myProjectPathField.getText();
-    if (myOriginalSettings == null) {
-      throw new ConfigurationException(String.format(
-        "Can't store external task settings into run configuration. Reason: target run configuration is undefined. Tasks: '%s', " +
-        "external project: '%s', vm options: '%s'",
-        myTasksTextField.getText(), projectPath, myVmOptionsEditor.getText()
-      ));
-    }
-    return true;
-  }
+	@Override
+	public boolean validate (@NotNull ExternalSystemTaskExecutionSettings settings) throws ConfigurationException {
+		String projectPath = myProjectPathField.getText();
+		if (myOriginalSettings == null) {
+			throw new ConfigurationException(
+				String.format(
+					"Can't store external task settings into run configuration. Reason: target run configuration is undefined. Tasks: '%s', "
+						+ "external project: '%s', vm options: '%s'", myTasksTextField.getText(), projectPath,
+					myVmOptionsEditor.getText()));
+		}
+		return true;
+	}
 
-  @Override
-  public void disposeUIResources() {
-    ExternalSystemUiUtil.disposeUi(this);
-  }
+	@Override
+	public void disposeUIResources () {
+		ExternalSystemUiUtil.disposeUi(this);
+	}
 
-  @Override
-  public void showUi(boolean show) {
-    ExternalSystemUiUtil.showUi(this, show);
-  }
+	@Override
+	public void showUi (boolean show) {
+		ExternalSystemUiUtil.showUi(this, show);
+	}
 }
