7a46359295cc8d7b2ab76d95456e42afd08b68ef
Darkyenus <darkyenus@gmail.com>
2018-01-18 14:28:03 +0100

    Allow to create projects and configurations dynamically
    Took 31 minutes

--- TODO.md
+++ TODO.md
@@ -4,14 +4,16 @@
 
 1. Trace debug system for debugging keys (+ print current key trace on first Ctrl-T/C and java stack trace on second Ctrl-T with same key trace)
 
+1. Allow to build IntelliJ plugins: https://github.com/JetBrains/gradle-intellij-plugin
+
 1. New project wizard (IDE)
 
 1. Allow projects to inherit from parent project configuration/config set
 	- Maybe simple "setup function" would be enough...
 
-1. IDE (IntelliJ, other Jetbrains later) integrations
+1. Hotswapping jvm code
 
-1. Allow to build IntelliJ plugins: https://github.com/JetBrains/gradle-intellij-plugin
+1. Android/MOE/RoboVM/TeaVM plugins
 
 1. Kotlin JS
 
--- src/main/kotlin/wemi/WemiDelegates.kt
+++ src/main/kotlin/wemi/WemiDelegates.kt
@@ -5,6 +5,7 @@ package wemi
 import org.slf4j.LoggerFactory
 import wemi.KeyDefaults.applyDefaults
 import wemi.boot.WemiBuildScript
+import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
 import kotlin.properties.ReadOnlyProperty
@@ -32,40 +33,58 @@ class ProjectDelegate internal constructor(
     private lateinit var project: Project
 
     operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): ProjectDelegate {
-        this.project = Project(property.name, projectRoot.toAbsolutePath())
-        synchronized(BuildScriptData.AllProjects) {
-            for ((_, project) in BuildScriptData.AllProjects) {
-                if (project.name == this.project.name) {
-                    throw WemiException("Project named '${this.project.name}' already exists (at ${project.projectRoot})")
-                }
-                if (project.projectRoot == this.project.projectRoot) {
-                    LOG.warn("Project ${this.project.name} is at the same location as project ${project.name}")
-                }
-            }
-            BuildScriptData.AllProjects.put(project.name, project)
-        }
-        this.project.apply {
-            Keys.projectName set { project.name }
-            Keys.projectRoot set { project.projectRoot }
-        }
-        this.project.applyDefaults()
-
-        // Setup Keys.buildScript
-        val buildScript = WemiBuildScript
-        if (buildScript == null) {
-            LOG.debug("Project {} is being initialized at unexpected time, introspection will not be available", this.project)
-        } else {
-            this.project.apply {
-                Keys.buildScript set { buildScript }
-            }
-        }
-
-        this.project.initializer()
-        this.project.locked = true
+        this.project = createProject(property.name, projectRoot, true, initializer)
         return this
     }
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): Project = project
+}
+
+/**
+ * ADVANCED USERS ONLY!!! Use [ProjectDelegate] instead for most uses.
+ *
+ * Create project dynamically, like using the [ProjectDelegate], but without the need to create a separate variable
+ * for each one.
+ *
+ * @param name of the project, must be unique
+ * @param root of the project
+ * @param checkRootUnique if true, check if no other project is in the [root] and warn if so
+ * @param initializer to populate the project's [BindingHolder] with bindings
+ */
+fun createProject(name:String, root:Path, checkRootUnique:Boolean = true, initializer: Project.() -> Unit):Project {
+    val usedRoot = root.toAbsolutePath()
+
+    val project = Project(name, usedRoot)
+    synchronized(BuildScriptData.AllProjects) {
+        for ((_, otherProject) in BuildScriptData.AllProjects) {
+            if (otherProject.name == name) {
+                throw WemiException("Project named '$name' already exists (at ${otherProject.projectRoot})")
+            }
+            if (checkRootUnique && Files.isSameFile(otherProject.projectRoot, usedRoot)) {
+                LOG.warn("Project $name is at the same location as project ${otherProject.name}")
+            }
+        }
+        BuildScriptData.AllProjects.put(project.name, project)
+    }
+    project.apply {
+        Keys.projectName set { name }
+        Keys.projectRoot set { usedRoot }
+
+        applyDefaults()
+
+        // Setup Keys.buildScript
+        val buildScript = WemiBuildScript
+        if (buildScript == null) {
+            LOG.debug("Project {} is being initialized at unexpected time, introspection will not be available", name)
+        } else {
+            Keys.buildScript set { buildScript }
+        }
+
+        initializer()
+        locked = true
+    }
+
+    return project
 }
 
 /**
@@ -111,19 +130,25 @@ class ConfigurationDelegate internal constructor(
     private lateinit var configuration: Configuration
 
     operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): ConfigurationDelegate {
-        this.configuration = Configuration(property.name, description, parent)
-        synchronized(BuildScriptData.AllConfigurations) {
-            val existing = BuildScriptData.AllConfigurations[configuration.name]
-            if (existing != null) {
-                throw WemiException("wemi.Configuration ${configuration.name} already exists (desc: '${existing.description}')")
-            }
-
-            BuildScriptData.AllConfigurations.put(configuration.name, configuration)
-        }
-        configuration.initializer()
-        configuration.locked = true
+        this.configuration = createConfiguration(property.name, description, parent, initializer)
         return this
     }
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): Configuration = configuration
+}
+
+fun createConfiguration(name:String, description: String, parent: Configuration?, initializer: Configuration.() -> Unit):Configuration {
+    val configuration = Configuration(name, description, parent)
+    synchronized(BuildScriptData.AllConfigurations) {
+        val existing = BuildScriptData.AllConfigurations[configuration.name]
+        if (existing != null) {
+            throw WemiException("wemi.Configuration ${configuration.name} already exists (desc: '${existing.description}')")
+        }
+
+        BuildScriptData.AllConfigurations.put(configuration.name, configuration)
+    }
+    configuration.initializer()
+    configuration.locked = true
+
+    return configuration
 }
