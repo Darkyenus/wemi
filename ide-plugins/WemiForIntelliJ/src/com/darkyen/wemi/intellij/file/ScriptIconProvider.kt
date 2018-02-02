73a7a914cf75afc1c67a2dfe5c7df8e78e85a9e6
Darkyenus <darkyenus@gmail.com>
2018-02-02 15:28:18 +0100

    Show icon for .wemi script files
    Took 33 minutes

--- ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/file/ScriptIconProvider.kt
+++ ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/file/ScriptIconProvider.kt
@@ -1,15 +1,32 @@
 package com.darkyen.wemi.intellij.file
 
 import com.intellij.ide.FileIconProvider
+import com.intellij.ide.IconProvider
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import icons.WemiIcons
 import javax.swing.Icon
 
 /**
- *
+ * Provides script icons for Wemi build scripts
  */
-class ScriptIconProvider : FileIconProvider {
+class ScriptIconProvider : IconProvider(), FileIconProvider, DumbAware  {
+
+    override fun getIcon(element: PsiElement, flags: Int): Icon? {
+        if ((element as? PsiFile)?.virtualFile?.isWemiScriptSource(true) == true) {
+            return WemiIcons.SCRIPT
+        }
+        return null
+    }
+
     override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
-        TODO("not implemented")
+        if (file.isWemiScriptSource(true)) {
+            return WemiIcons.SCRIPT
+        }
+
+        return null
     }
 }
--- ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/file/Util.kt
+++ ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/file/Util.kt
@@ -7,19 +7,33 @@ import com.intellij.openapi.vfs.VirtualFile
 /**
  *
  */
-fun VirtualFile?.isWemiLauncher():Boolean {
+internal fun VirtualFile?.isWemiLauncher():Boolean {
     if (this == null || this.isDirectory) {
         return false
     }
     return this.name == WemiLauncherFileName
 }
 
-fun VirtualFile?.isWemiScriptSource():Boolean {
+internal fun VirtualFile?.isWemiScriptSource(deepCheck:Boolean = false):Boolean {
     if (this == null || this.isDirectory || this.name.startsWith('.')) {
         return false
     }
 
-    return WemiBuildFileExtensions.contains(this.extension?.toLowerCase() ?: "")
+    if (!WemiBuildFileExtensions.contains(this.extension?.toLowerCase() ?: "")) {
+        return false
+    }
+
+    if (deepCheck) {
+        val buildDirectory = this.parent ?: return false
+        if (!buildDirectory.name.equals("build", ignoreCase = true)) {
+            return false
+        }
+        val projectDirectory = buildDirectory.parent ?: return false
+        val wemiLauncher = projectDirectory.findChild(WemiLauncherFileName) ?: return false
+        return wemiLauncher.isWemiLauncher()
+    }
+
+    return false
 }
 
 /**
@@ -27,7 +41,7 @@ fun VirtualFile?.isWemiScriptSource():Boolean {
  *
  * Not case sensitive.
  */
-fun String.pathHasExtension(extensions: Iterable<String>): Boolean {
+internal fun String.pathHasExtension(extensions: Iterable<String>): Boolean {
     val name = this
     val length = name.length
     for (extension in extensions) {
