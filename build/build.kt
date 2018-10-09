c6cac758ed313809fab7e38c5daec2f0e7d91083
Darkyenus <darkyenus@gmail.com>
2018-10-09 14:47:42 +0200

    Version 0.5
    Took 9 minutes

--- build/build.kt
+++ build/build.kt
@@ -24,7 +24,7 @@ val CompilerProjects = listOf(
 )
 
 private const val WemiGroup = "com.darkyen.wemi"
-private const val WemiVersion = "0.5-SNAPSHOT"
+private const val WemiVersion = "0.5"
 
 /**
  * Wemi Build System core
