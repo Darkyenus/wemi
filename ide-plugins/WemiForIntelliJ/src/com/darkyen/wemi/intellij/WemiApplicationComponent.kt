6b5f345bcf8e198cb93001393135b6c9d0ef53e3
Darkyenus <darkyenus@gmail.com>
2019-06-27 11:16:14 +0200

    [intellij] Wrong thread fix
    Took 7 hours 3 minutes

--- ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/WemiApplicationComponent.kt
+++ ide-plugins/WemiForIntelliJ/src/com/darkyen/wemi/intellij/WemiApplicationComponent.kt
@@ -1,6 +1,7 @@
 package com.darkyen.wemi.intellij
 
 import com.darkyen.wemi.intellij.file.WemiLauncherFileType
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.BaseComponent
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants.USE_IN_PROCESS_COMMUNICATION_REGISTRY_KEY_SUFFIX
 import com.intellij.openapi.fileTypes.ExactFileNameMatcher
@@ -16,11 +17,15 @@ class WemiApplicationComponent : BaseComponent {
     // TODO(jp): Do what https://github.com/JetBrains/intellij-community/blob/master/platform/core-api/src/com/intellij/openapi/components/ApplicationComponent.java says
 
     override fun initComponent() {
-        FileTypeManager.getInstance().associate(WemiLauncherFileType, ExactFileNameMatcher(WemiLauncherFileName, false))
+        ApplicationManager.getApplication().invokeLater {
+            ApplicationManager.getApplication().runWriteAction {
+                FileTypeManager.getInstance().associate(WemiLauncherFileType, ExactFileNameMatcher(WemiLauncherFileName, false))
 
-        // Do not launch our WemiProjectResolver and WemiTaskManager in external process,
-        // because it just adds delays and is messy
-        Registry.get("${WemiProjectSystemId.id}$USE_IN_PROCESS_COMMUNICATION_REGISTRY_KEY_SUFFIX").setValue(true)
+                // Do not launch our WemiProjectResolver and WemiTaskManager in external process,
+                // because it just adds delays and is messy
+                Registry.get("${WemiProjectSystemId.id}$USE_IN_PROCESS_COMMUNICATION_REGISTRY_KEY_SUFFIX").setValue(true)
+            }
+        }
     }
 
     override fun getComponentName(): String {
