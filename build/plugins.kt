97fb33397285c0a506ef212c744b3920621915b4
Darkyenus <darkyenus@gmail.com>
2020-10-14 01:35:18 +0200

    [TeaVM] Add initial TeaVM plugin
    Took 38 minutes

--- build/plugins.kt
+++ build/plugins.kt
@@ -1,4 +1,7 @@
 import wemi.dependency.ProjectDependency
+import wemi.generation.Constant
+import wemi.generation.generateKotlinConstantsFile
+import wemi.generation.generateSources
 
 /** Plugin for hotswapping JVM code at runtime. */
 val pluginJvmHotswap by project(path("plugins/jvm-hotswap")) {
@@ -15,6 +18,33 @@ val pluginJvmHotswap by project(path("plugins/jvm-hotswap")) {
                 metadata,
                 "Wemi Plugin: JVM Hotswap",
                 "Adds JVM code hotswap integration for Wemi Build System",
+                "2018"
+        )
+    }
+}
+
+val pluginTeaVM by project(path("plugins/teavm")) {
+    projectGroup set { WemiGroup }
+    projectName set { "wemi-plugin-teavm" }
+    projectVersion set { using(wemi) { projectVersion.get() } }
+
+    val TEAVM_VERSION = "0.6.1"
+
+    generateSources {
+        generateKotlinConstantsFile(it, "wemiplugin.teavm.Version",
+                mapOf("TEAVM_VERSION" to Constant.StringConstant(TEAVM_VERSION, "The version of TeaVM to be used")))
+    }
+
+    projectDependencies add { ProjectDependency(core, false, scope=ScopeProvided) }
+    libraryDependencies add { dependency("org.teavm", "teavm-tooling", TEAVM_VERSION) }
+    libraryDependencies add { Dependency(JUnitAPI, scope=ScopeTest) }
+    libraryDependencies add { Dependency(JUnitEngine, scope=ScopeTest) }
+
+    publishMetadata modify { metadata ->
+        setupSharedPublishMetadata(
+                metadata,
+                "Wemi Plugin: TeaVM support",
+                "Compile Java bytecode to JavaScript using TeaVM",
                 "2018"
         )
     }
