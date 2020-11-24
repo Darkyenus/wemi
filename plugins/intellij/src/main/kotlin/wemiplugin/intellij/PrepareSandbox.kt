5fdcc4f8016e82d403d534a6d2221e4afefa7a39
Darkyenus <darkyenus@gmail.com>
2020-11-24 10:22:55 +0100

    [intellij] Do not delete all sandbox plugins on rebuild, only those which we added
    Took 11 minutes

--- plugins/intellij/src/main/kotlin/wemiplugin/intellij/PrepareSandbox.kt
+++ plugins/intellij/src/main/kotlin/wemiplugin/intellij/PrepareSandbox.kt
@@ -6,14 +6,17 @@ import org.slf4j.LoggerFactory
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import wemi.EvalScope
+import wemi.util.deleteRecursively
 import wemi.util.div
 import wemi.util.ensureEmptyDirectory
+import wemi.util.exists
 import wemi.util.linkOrCopyRecursively
 import wemi.util.name
 import wemiplugin.intellij.utils.namedElements
 import wemiplugin.intellij.utils.parseXml
 import wemiplugin.intellij.utils.saveXml
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import javax.xml.parsers.DocumentBuilderFactory
 
 private val LOG = LoggerFactory.getLogger("PrepareSandbox")
@@ -26,20 +29,38 @@ class IntelliJIDESandbox(val base:Path, val config:Path, val plugins:Path, val s
 
 fun EvalScope.prepareIntelliJIDESandbox(sandboxDir: Path = Keys.cacheDirectory.get() / "idea-sandbox", testSuffix:String = "", vararg extraPluginDirectories: Path): IntelliJIDESandbox {
 	val destinationDir = sandboxDir / "plugins$testSuffix"
-	destinationDir.ensureEmptyDirectory()
+
+	// A file which contains names of files added by us to clear.
+	// We do not want to delete everything, because user might have added custom plugins from marketplace etc.
+	val customContentManifest = destinationDir / "wemiContentManifest.txt"
+	if (customContentManifest.exists()) {
+		for (line in Files.readAllLines(customContentManifest)) {
+			val dir = destinationDir / line.trim()
+			dir.deleteRecursively()
+		}
+	} else {
+		destinationDir.ensureEmptyDirectory()
+	}
+
+	val customContentNames = ArrayList<String>()
 
 	val pluginJar = IntelliJ.intellijPluginFolder.get()
 	pluginJar.linkOrCopyRecursively(destinationDir / pluginJar.name)
+	customContentNames.add(pluginJar.name)
 
 	for (dependency in IntelliJ.intellijResolvedPluginDependencies.get()) {
 		if (dependency.isBuiltin) {
 			continue
 		}
 		dependency.artifact.linkOrCopyRecursively(destinationDir / dependency.artifact.name)
+		customContentNames.add(dependency.artifact.name)
 	}
 	for (path in extraPluginDirectories) {
 		path.linkOrCopyRecursively(destinationDir / path.name)
+		customContentNames.add(path.name)
 	}
+
+	Files.write(customContentManifest, customContentNames, Charsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)
 
 	// Disable IDE update
 	val configDir = sandboxDir / "config$testSuffix"
